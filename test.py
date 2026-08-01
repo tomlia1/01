e3871463f12006889479aeb567f22c55d0b58492
tomlia1 <1783486196@qq.com>
2026-08-01 18:10:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ user_data = {
 print(user_data["target_job"])  # 键名是 "target_job"，有下划线
 # 需求：如果自学时间大于等于 4 小时，打印合格；否则打印偷懒
 study_hours = 5
-
 if study_hours >= 4:  # == 是比较，= 是赋值；>= 才是"大于等于"
     print("今日自学达标，抗压成功！")
 else:
